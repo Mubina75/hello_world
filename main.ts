69eebf7928969b40748a8d5f8d86d0b56017e177
Mubina75 <122555210+Mubina75@users.noreply.github.com>
2023-01-13 12:23:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
+    music.playMelody("C D E F G A B C5 ", 120)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+    music.playMelody("B A F D F G B C5 ", 120)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.SmallHeart)
+    music.playMelody("A D C5 G C A F B ", 274)
 })
-basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Giraffe)
